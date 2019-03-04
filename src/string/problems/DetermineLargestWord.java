5f24fa1c60a1a87b4be9bc89db86a7d6bff90d26
margarita <Anthony Hazoury>
2019-03-03 23:01:31 -0500

    string.problems

--- src/string/problems/DetermineLargestWord.java
+++ src/string/problems/DetermineLargestWord.java
@@ -9,10 +9,20 @@ public class DetermineLargestWord {
          Implement to Find the length and longest word in the given sentence below.
          Should return "10 biological".
          */
+
         String s="Human brain is a biological learning machine";
         Map<Integer, String> wordNLength = findTheLargestWord(s);
         //implement
 
+        String [] word = s.split(" ");
+        String maxlethWord = "";
+
+        for(int i = 0; i < word.length; i++){
+            if(word[i].length() >= maxlethWord.length()){
+                maxlethWord = word[i];
+            }
+        }
+        System.out.println(maxlethWord);
     }
 
     public static Map<Integer, String> findTheLargestWord(String wordGiven){
--- src/string/problems/UnitTestingStringProblem.java
+++ src/string/problems/UnitTestingStringProblem.java
@@ -7,5 +7,7 @@ public class UnitTestingStringProblem {
         //Apply Unit Test into all the methods in this package.
 
 
+
     }
 }
+
